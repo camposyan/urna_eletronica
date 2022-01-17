665674f8bf039797055f94a06c754291ed446290
Yan Campos <camposyan00@gmail.com>
2022-01-16 22:33:22 -0300

    feat: layout da urna finalizado

---
+++ javascript/etapas.js
@@ -0,0 +1,70 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numero: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: "Antônio Aristeu",
+        partido: "PBL",
+        fotos: [
+          {
+            url: "sem-imagem.png",
+            legenda: "Vereador",
+            small: false,
+          },
+        ],
+      },
+      {
+        numero: 98182,
+        nome: "Pacheco Silva",
+        partido: "PLL",
+        fotos: [
+          {
+            url: "sem-imagem.png",
+            legenda: "Vereador",
+            small: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        nome: "Marcos Paschoal",
+        partido: "PLL",
+        fotos: [
+          {
+            url: "sem-imagem.png",
+            legenda: "Prefeito",
+            small: false,
+          },
+          {
+            url: "sem-imagem.png",
+            legenda: "Vice-prefeito",
+            small: true,
+          },
+        ],
+        numero: 41,
+        nome: "Thereza Soares das Graças",
+        partido: "PJR",
+        fotos: [
+          {
+            url: "sem-imagem.png",
+            legenda: "Prefeito",
+            small: false,
+          },
+          {
+            url: "sem-imagem.png",
+            legenda: "Vice-prefeito",
+            small: true,
+          },
+        ],
+      },
+    ],
+  },
+];
